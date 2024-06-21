193072cccdf272576d6e6410774a9b735aec4928
cbasil <cbasil@126.com>
2024-06-21 14:38:49 +0800

    重构Int64toString

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"goblog/pkg/logger"
 	"goblog/pkg/route"
+	"goblog/pkg/types"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -69,17 +70,13 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.New("show.gohtml").
 			Funcs(template.FuncMap{
 				"RouteName2URL": route.RouteName2URL,
-				"Int64ToString": Int64ToString,
+				"Int64ToString": types.Int64ToString,
 			}).ParseFiles("resources/views/articles/show.gohtml")
 
 		logger.LogError(err)
 		err = tmpl.Execute(w, article)
 		logger.LogError(err)
 	}
-}
-
-func Int64ToString(num int64) string {
-	return strconv.FormatInt(num, 10)
 }
 
 type ArticlesFormData struct {
