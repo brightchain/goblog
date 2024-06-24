7d54eab5a1a019a3f90e26ccf6714f3e3bcdfe62
cbasil <cbasil@126.com>
2024-06-24 15:09:07 +0800

    优化

--- main.go
+++ main.go
@@ -1,45 +1,14 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
-	"goblog/pkg/database"
 	"net/http"
-	"strings"
-
-	"github.com/gorilla/mux"
 )
 
-func forchHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-var db *sql.DB
-var router *mux.Router
-
 func main() {
-	database.Initialize()
-	db = database.DB
-
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
-	router.Use(forchHTMLMiddleware)
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL: ", homeURL)
-	articleURL, _ := router.Get("articles.show").URL("id", "23")
-	fmt.Println("articleURL: ", articleURL)
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	router := bootstrap.SetupRoute()
+
+	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
--- routes/web.go
+++ routes/web.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"goblog/app/http/controllers"
+	"goblog/app/http/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -13,11 +14,12 @@ func RegisterWebRoutes(r *mux.Router) {
 	r.HandleFunc("/about", pc.About).Methods("GET").Name("about")
 	r.NotFoundHandler = http.HandlerFunc(pc.NotFound)
 	ac := new(controllers.ArticleControllers)
-	r.HandleFunc("/articles/index", ac.Index).Methods("GET").Name("articles.index")
+	r.HandleFunc("/articles", ac.Index).Methods("GET").Name("articles.index")
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Show).Methods("GET").Name("articles.show")
 	r.HandleFunc("/articles", ac.Store).Methods("POST").Name("articles.store")
 	r.HandleFunc("/articles/create", ac.Create).Methods("GET").Name("articles.create")
 	r.HandleFunc("/articles/{id:[0-9]+}/edit", ac.Edit).Methods("GET").Name("articles.edit")
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Update).Methods("POST").Name("articles.update")
 	r.HandleFunc("/articles/{id:[0-9]+}/delete", ac.Delete).Methods("POST").Name("articles.delete")
+	r.Use(middlewares.ForceHTML)
 }
