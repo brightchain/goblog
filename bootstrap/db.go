46a73bf64b65bab480f00f797099410e91a722ef
cbasil <cbasil@126.com>
2024-07-01 17:02:06 +0800

    创建用户

--- bootstrap/db.go
+++ bootstrap/db.go
@@ -1,8 +1,12 @@
 package bootstrap
 
 import (
+	"goblog/app/models/article"
+	"goblog/app/models/user"
 	"goblog/pkg/model"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 func SetupDB() {
@@ -11,4 +15,14 @@ func SetupDB() {
 	sqlDB.SetMaxOpenConns(100)
 	sqlDB.SetMaxIdleConns(25)
 	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+	migration(db)
+}
+
+func migration(db *gorm.DB) {
+
+	// 自动迁移
+	db.AutoMigrate(
+		&user.User{},
+		&article.Article{},
+	)
 }
