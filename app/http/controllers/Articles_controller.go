c1cc1f60fd645c54d8faa04d9a251c466a0ee467
cbasil <cbasil@126.com>
2024-07-02 10:59:05 +0800

    用户登录状态

--- app/http/controllers/Articles_controller.go
+++ app/http/controllers/Articles_controller.go
@@ -22,7 +22,9 @@ func (*ArticleControllers) Index(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, "500 服务器内部错误")
 	} else {
-		view.Render(w, articles, "articles.index")
+		view.Render(w, view.D{
+			"Articles": articles,
+		}, "articles.index")
 	}
 }
 
@@ -40,7 +42,9 @@ func (*ArticleControllers) Show(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "500 服务器内部错误")
 		}
 	} else {
-		view.Render(w, article, "articles.show")
+		view.Render(w, view.D{
+			"Article": article,
+		}, "articles.show")
 	}
 }
 
--- pkg/view/view.go
+++ pkg/view/view.go
@@ -1,6 +1,7 @@
 package view
 
 import (
+	"goblog/pkg/auth"
 	"goblog/pkg/logger"
 	"goblog/pkg/route"
 	"html/template"
@@ -11,24 +12,18 @@ import (
 
 type D map[string]interface{}
 
-func Render(w io.Writer, data interface{}, tplFiles ...string) {
+func Render(w io.Writer, data D, tplFiles ...string) {
 	RenderTemplate(w, "app", data, tplFiles...)
 }
 
-func RenderSimple(w io.Writer, data interface{}, tplFiles ...string) {
+func RenderSimple(w io.Writer, data D, tplFiles ...string) {
 	RenderTemplate(w, "simple", data, tplFiles...)
 }
 
-func RenderTemplate(w io.Writer, name string, data interface{}, tplFiles ...string) {
-	viewDir := "resources/views/"
-	for i, f := range tplFiles {
-		tplFiles[i] = viewDir + strings.Replace(f, ".", "/", -1) + ".gohtml"
-	}
+func RenderTemplate(w io.Writer, name string, data D, tplFiles ...string) {
+	data["isLogined"] = auth.Check()
 
-	layoutFiles, err := filepath.Glob(viewDir + "layouts/*.gohtml")
-	logger.LogError(err)
-
-	allFiles := append(layoutFiles, tplFiles...)
+	allFiles := getTemplateFiles(tplFiles...)
 
 	tmpl, err := template.New("").
 		Funcs(template.FuncMap{
@@ -38,4 +33,21 @@ func RenderTemplate(w io.Writer, name string, data interface{}, tplFiles ...stri
 
 	err = tmpl.ExecuteTemplate(w, name, data)
 	logger.LogError(err)
+}
+
+func getTemplateFiles(tplFiles ...string) []string {
+	// 1 设置模板相对路径
+	viewDir := "resources/views/"
+
+	// 2. 遍历传参文件列表 Slice，设置正确的路径，支持 dir.filename 语法糖
+	for i, f := range tplFiles {
+		tplFiles[i] = viewDir + strings.Replace(f, ".", "/", -1) + ".gohtml"
+	}
+
+	// 3. 所有布局模板文件 Slice
+	layoutFiles, err := filepath.Glob(viewDir + "layouts/*.gohtml")
+	logger.LogError(err)
+
+	// 4. 合并所有文件
+	return append(layoutFiles, tplFiles...)
 }
