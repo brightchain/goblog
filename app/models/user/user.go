518a0a498b0a6919bfdf04916c696b51a8f0ea36
cbasil <cbasil@126.com>
2024-07-02 11:43:21 +0800

    用户密码加密

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -1,6 +1,9 @@
 package user
 
-import "goblog/app/models"
+import (
+	"goblog/app/models"
+	"goblog/pkg/password"
+)
 
 type User struct {
 	models.BaseModel
@@ -10,6 +13,6 @@ type User struct {
 	PasswordConfirm string `gorm:"-" valid:"password_confirm"`
 }
 
-func (user *User) ComparePassword(password string) bool {
-	return user.Password == password
+func (user *User) ComparePassword(_password string) bool {
+	return password.CheckHash(_password, user.Password)
 }
